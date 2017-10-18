5609487fd59e5eb499bd1843ea0b4d09c9d06404
Konstantin Gulyy <kgulyy@my.com>
2017-10-19 00:23:52 +0300

    Add context load test

---
+++ src/test/java/ru/mail/park/gwent/ApplicationTest.java
@@ -0,0 +1,18 @@
+package ru.mail.park.gwent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Konstantin Gulyy
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
