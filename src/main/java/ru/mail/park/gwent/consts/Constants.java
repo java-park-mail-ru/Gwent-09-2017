689df739fc23865355d54f81654db19a53191f1b
Konstantin Gulyy <kgulyy@my.com>
2017-12-14 00:21:23 +0300

    Update origin https

--- src/main/java/ru/mail/park/gwent/consts/Constants.java
+++ src/main/java/ru/mail/park/gwent/consts/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final String AUTH_URL = "/api/auth";
     public static final String USERS_URL = "/api/users";
 
-    public static final String ORIGIN = "http://tech-gwent.xyz";
+    public static final String ORIGIN = "https://tech-gwent.xyz";
     public static final String LOCALHOST = "http://localhost:8000";
     public static final String GWENT_FRONT = "https://gwent-front.herokuapp.com";
     public static final String TECH_GWENT = "https://techgwent.herokuapp.com";
